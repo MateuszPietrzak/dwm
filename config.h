3b8d673fa6f4cd0bc0dbaf16393961c044412898
MateuszPietrzak <mateusz.jerzy.pietrzak@gmail.com>
2022-11-02 19:05:08 +0100

    barheight

--- config.h
+++ config.h
@@ -15,8 +15,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
+static const int user_bh            = 26;
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#32302f";
 static const char col_gray2[]       = "#282828";
 static const char col_gray3[]       = "#928374";
@@ -29,7 +30,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "dev", "web", "msg", "music", "games", "6", "7", "8", "9" };
+static const char *tags[] = { "1 dev", "2 web", "3 msg", "4 music", "5 launchers", "6 games", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -37,14 +38,18 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class             instance    title              tags mask     isfloating   monitor */
-	{ "sxiv",            NULL,       NULL,              0,            1,           -1 },
+	{ "Sxiv",            NULL,       NULL,              0,            1,           -1 },
 	{ "jetbrains-clion", NULL,       NULL,              1<<0,         0,           -1 },
-	{ "vscodium",        NULL,       NULL,              1<<0,         0,           -1 },
+	{ "VSCodium",        NULL,       NULL,              1<<0,         0,           -1 },
 	{ "firefox",         NULL,       NULL,              1<<1,         0,           -1 },
 	{ "discord",         NULL,       NULL,              1<<2,         0,           -1 },
-	{ "spotify",         NULL,       NULL,              1<<3,         0,           -1 },
+	{ "Spotify",         "spotify",  "Spotify",         1<<3,         0,           -1 },
 	{ "Steam",           NULL,       NULL,              1<<4,         0,           -1 },
-	{ "gdlauncher",      NULL,       NULL,              1<<4,         0,           -1 }
+	{ "gdlauncher",      NULL,       NULL,              1<<4,         0,           -1 },
+	{ "Minecraft",       NULL,       NULL,              1<<5,         0,           -1 },
+	{ "steam_app",       NULL,       NULL,              1<<5,         0,           -1 },
+	{ "hl2_linux",       NULL,       NULL,              1<<5,         0,           -1 }
+
 };
 
 /* layout(s) */
